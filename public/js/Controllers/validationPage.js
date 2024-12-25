f78d984c634658ed772596a0863fce2a64fe3c81
coronelhernan <hcoronel1996@gmail.com>
2024-12-25 18:53:16 -0300

    Cambios generados en la estructura de los archivos public

--- public/js/Controllers/validationPage.js
+++ public/js/Controllers/validationPage.js
@@ -4,13 +4,12 @@ document.addEventListener("DOMContentLoaded", () => {
     "/seguridadDigital",
     "/ataquesAlDominio",
     "/documentosElectronicos",
-    "/contratosInformaticos"
+    "/contratosInformaticos",
   ];
 
   const currentPath = window.location.pathname;
 
   if (!validRoutes.includes(currentPath)) {
-    // Redirigir a la pagina 404 si no existe una pagina 
     window.history.replaceState({}, "", "/404");
     window.location.href = "/404";
   }

05ca51b93438e762cc17669e11fa34d9b37ef66a
coronelhernan <hcoronel1996@gmail.com>
2024-12-26 19:08:45 -0300

    Actualizacion header y nav responsive

--- public/js/Controllers/buttonHeader.js
+++ public/js/Controllers/buttonHeader.js
@@ -1,13 +1,24 @@
 const d = document;
+const containLogo = d.getElementById('contain-logo');
 const btnMenu = d.getElementById('btn-menu');
 const menu = d.getElementById('menu');
+const menuIcon = d.getElementById('icon-btn');
 const menuLinks = menu ? menu.querySelectorAll('a') : [];
 
 // Verificar si el botón de menú y el menú están disponibles
 if (btnMenu && menu) {
-  // Logica para activar y desactivar la visibilidad del menú
+  // Lógica para activar y desactivar la visibilidad del menú y alternar el ícono
   btnMenu.addEventListener('click', () => {
-    menu.classList.toggle('translate-x-full'); 
+    menu.classList.toggle('translate-x-full');
+    if (menuIcon.classList.contains('fa-bars')) {
+      menuIcon.classList.remove('fa-bars');
+      menuIcon.classList.add('fa-xmark');
+      containLogo.classList.add('hidden-element');
+    } else {
+      menuIcon.classList.remove('fa-xmark');
+      menuIcon.classList.add('fa-bars');
+      containLogo.classList.remove('hidden-element')
+    }
   });
 }
 
@@ -17,6 +28,8 @@ if (menuLinks.length > 0) {
     link.addEventListener('click', () => {
       // Agregar la clase para ocultar el menú
       menu.classList.add('translate-x-full');
+      menuIcon.classList.remove('fa-xmark');
+      menuIcon.classList.add('fa-bars');
     });
   });
 }
